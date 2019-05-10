ee4b9ad43da3e9150516bc9127c8a13228fd4003
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-05-10 15:47:53 -0400

    Update loader32.c

--- loader32.c
+++ loader32.c
@@ -1,9 +1,6 @@
 #include <Windows.h>
 
 int main() {
-    printf("Running shellcode\n");
-    
-    // Shellcode format is 0x90, 0x90
     asm("call code\n\t"
         ".byte {{SHELLCODE}}\n\t" 
         "code:\n\t"
