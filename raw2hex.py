8208be6ffd591c13672506c67d27001dfa4d7039
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-05-10 15:35:54 -0400

    Create raw2hex.py

---
+++ raw2hex.py
@@ -0,0 +1,21 @@
+import binascii
+import sys
+import re
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) < 2:
+		print("Missing file path")
+		sys.exit(0)
+		
+	try:
+		data = binascii.b2a_hex(open(sys.argv[1], "rb").read()).decode()
+	except:
+		print("Error reading %s" % sys.argv[1])
+		print(sys.exc_info()[1])
+		sys.exit(0)
+		
+	if "-list" in sys.argv:
+		print("0x" + ",0x".join(re.findall("..", data)))
+	else:
+		print("\\x" + "\\x".join(re.findall("..", data)))
